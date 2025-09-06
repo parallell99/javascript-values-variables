5d241b079e3cadaa020f2b11578b665e40cea8fe
parallell99 <jr.projectx@gmail.com>
2025-09-06 19:06:46 +0700

    ex1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,13 +1,13 @@
 //Exercise #1: Variable Creation
-let name = "Karter"
-let lastName = "Anderson"
-let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-const restaurant = "Chef's Table by Chef James"
-const foodBill = 15000
-const totalRestaurants = 1500
+const restaurant = "Chef's Table by Chef James";
+const foodCoast = 15000;
+const totalRestaurants = 1500;
 
 //Exercise #2: Variable Reassignment
-let address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
-const foodBill = 5000
-const totalRestaurants = 2500
+let addressNew = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+let totalFoodCoast = 5000;
+let totalOfRestaurants = 2500;
