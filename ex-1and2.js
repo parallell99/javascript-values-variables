a9649aebad05b4c508048ca140e40ee87a0d4dc6
parallell99 <jr.projectx@gmai.com>
2025-09-06 18:24:14 +0700

    ex1-2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const restaurant = "Chef's Table by Chef James"
+const foodBill = 15000
+const totalRestaurants = 1500
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+let address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+const foodBill = 5000
+const totalRestaurants = 2500
